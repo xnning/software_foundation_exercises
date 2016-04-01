144f6ebe65ddc3c30be0a8ac3736406a9bb72c05
ningning <xnningxie@gmail.com>
2016-04-01 10:29:32 +0800

    Using Evidence in Proofs.

--- Prop.v
+++ Prop.v
@@ -68,3 +68,168 @@ Proof.
   simpl. apply b_0.
   simpl. apply b_sum. apply B. apply IHm'.
 Qed.
+
+(* Using Evidence in Proofs *)
+
+(* Induction over Evidence *)
+Inductive gorgeous : nat -> Prop :=
+  g_0 : gorgeous 0
+| g_plus3 : forall n, gorgeous n -> gorgeous (3+n)
+| g_plus5 : forall n, gorgeous n -> gorgeous (5+n).
+
+(* Exercise: 1 star (gorgeous_plus13) *)
+
+Theorem gorgeous_plus13: forall n,
+  gorgeous n ->  gorgeous (13+n).
+Proof.
+  intros n H.
+  apply g_plus3. apply g_plus5. apply g_plus5. apply H.
+Qed.
+
+Theorem gorgeous__beautiful_FAILED : forall n,
+  gorgeous n -> beautiful n.
+Proof.
+   intros. induction n as [| n'].
+   Case "n = 0". apply b_0.
+   Case "n = S n'". (* We are stuck! *)
+Abort.
+
+Theorem gorgeous__beautiful : forall n,
+  gorgeous n -> beautiful n.
+Proof.
+   intros n H.
+   induction H as [|n'|n'].
+   Case "g_0".
+       apply b_0.
+   Case "g_plus3".
+       apply b_sum. apply b_3.
+       apply IHgorgeous.
+   Case "g_plus5".
+       apply b_sum. apply b_5. apply IHgorgeous.
+Qed.
+
+(* Exercise: 2 stars (gorgeous_sum) *)
+
+Theorem gorgeous_sum : forall n m,
+  gorgeous n -> gorgeous m -> gorgeous (n + m).
+Proof.
+  intros.
+  induction H as [|n'|n'].
+  apply H0.
+  rewrite <- plus_assoc. apply g_plus3. apply IHgorgeous.
+  rewrite <- plus_assoc. apply g_plus5. apply IHgorgeous.
+Qed.
+
+(* Exercise: 3 stars, advanced (beautiful__gorgeous) *)
+
+Theorem beautiful__gorgeous : forall n, beautiful n -> gorgeous n.
+Proof.
+  intros.
+  induction H as [| | | n1 n2].
+  Case "n = 0". apply g_0.
+  Case "n = 3". apply g_plus3. apply g_0.
+  Case "n = 5". apply g_plus5. apply g_0.
+  Case "n = n1 + n2". apply gorgeous_sum. apply IHbeautiful1. apply IHbeautiful2.
+Qed.
+
+(* Exercise: 3 stars, optional (g_times2) *)
+Lemma helper_g_times2 : forall x y z, x + (z + y) = z + x + y.
+Proof.
+  intros.
+  rewrite -> plus_assoc.
+  rewrite -> (plus_comm x z).
+  reflexivity.
+Qed.
+
+Theorem g_times2: forall n, gorgeous n -> gorgeous (2×n).
+Proof.
+   intros n H. simpl. rewrite <- plus_n_O.
+   induction H.
+   Case "n = 0". apply g_0.
+   Case "n = 3 + n".
+     rewrite helper_g_times2. apply g_plus3 in IHgorgeous.
+     apply g_plus3 in IHgorgeous. apply IHgorgeous.
+   Case "n = 5 + n".
+     rewrite helper_g_times2. apply g_plus5 in IHgorgeous.
+     apply g_plus5 in IHgorgeous. apply IHgorgeous.
+Qed.
+
+Theorem ev__even : forall n,
+  ev n -> even n.
+Proof.
+  intros n E. induction E as [| n' E'].
+  Case "E = ev_0".
+    unfold even. reflexivity.
+  Case "E = ev_SS n' E'".
+    unfold even. apply IHE'.
+Qed.
+
+Theorem ev_sum : forall n m,
+   ev n -> ev m -> ev (n+m).
+Proof.
+  intros.
+  induction H.
+  apply H0.
+  rewrite -> plus_Sn_m.
+  rewrite -> plus_Sn_m.
+  apply ev_SS. apply IHev.
+Qed.
+
+(* Inversion on Evidence *)
+
+Theorem ev_minus2: forall n, ev n -> ev (pred (pred n)).
+Proof.
+  intros n E.
+  inversion E as [| n' E'].
+  Case "E = ev_0". simpl. apply ev_0.
+  Case "E = ev_SS n' E'". simpl. apply E'. Qed.
+
+Theorem SSev__even : forall n,
+  ev (S (S n)) -> ev n.
+Proof.
+  intros n E.
+  inversion E as [| n' E'].
+  apply E'. Qed.
+
+(* The Inversion Tactic Revisited *)
+
+(* Exercise: 1 star (inversion_practice) *)
+
+Theorem SSSSev__even : forall n,
+  ev (S (S (S (S n)))) -> ev n.
+Proof.
+  intros.
+  inversion H.
+  inversion H1.
+  apply H3.
+Qed.
+
+Theorem even5_nonsense :
+  ev 5 ->  2 + 2 = 9.
+Proof.
+  intros.
+  inversion H. inversion H1. inversion H3.
+Qed.
+
+(* Exercise: 3 stars, advanced (ev_ev__ev) *)
+
+Theorem ev_ev__ev : forall n m,
+  ev (n+m) -> ev n -> ev m.
+Proof.
+  intros n m H1 H2.
+  induction H2 as[ |n'].
+  Case "n = 0". simpl in H1. apply H1.
+  Case "n = S (S n')".
+    inversion H1. apply IHev in H0. apply H0.
+Qed.
+
+(* Exercise: 3 stars, optional (ev_plus_plus) *)
+Theorem ev_plus_plus : forall n m p,
+  ev (n+m) -> ev (n+p) -> ev (m+p).
+Proof.
+  intros.
+  assert (ev (n + m + (n + p))). apply ev_sum. apply H. apply H0.
+  rewrite plus_swap in H1. rewrite plus_assoc in H1. rewrite plus_assoc in H1.
+  rewrite <- double_plus in H1. rewrite <- plus_assoc in H1.
+  apply ev_ev__ev with (n:= double n). apply H1. apply double_even.
+Qed.
